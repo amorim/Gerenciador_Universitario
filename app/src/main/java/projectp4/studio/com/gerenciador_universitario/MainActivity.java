86a8b046e267a1da1e693343dd2f51e3cef5a63a
Lucas Sales <ldss@ic.ufal.br>
2018-05-01 02:48:08 -0300

    ProjetoP4
    ainda-testando

--- app/src/main/java/projectp4/studio/com/gerenciador_universitario/MainActivity.java
+++ app/src/main/java/projectp4/studio/com/gerenciador_universitario/MainActivity.java
@@ -1,6 +1,9 @@
 package projectp4.studio.com.gerenciador_universitario;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +14,7 @@ public class MainActivity extends Activity {
 
     private Button notas;
     private Button faltas;
+    private Button addMat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class MainActivity extends Activity {
 
         notas = (Button) findViewById(R.id.notas);
         faltas = (Button) findViewById(R.id.faltas);
+        addMat = (Button) findViewById(R.id.addMat);
 
         notas.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -31,6 +36,21 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, MainFaltas.class));
+            }
+        });
+
+        addMat.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentManager frag = getFragmentManager();
+                FragmentTransaction ftrans = frag.beginTransaction();
+                AddMat f = new AddMat();
+
+                ftrans.add(R.id.rl_layout, f);
+
+                ftrans.commit();
+
+
             }
         });
 
